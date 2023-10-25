dc431890ebd4f925a18536351b594c4b83abf888
apinx <me@apinx.dk>
2023-10-25 19:47:43 +0200

    Day 16

--- days/src/main/kotlin/day16/Day16.kt
+++ days/src/main/kotlin/day16/Day16.kt
@@ -1,9 +1,37 @@
 package day16
 
-fun task1(input: List<String>): Int {
-    return -1
+fun buildNext(a: String): String {
+    val b = a.reversed().replace('0', '2').replace('1', '0').replace('2', '1')
+    return "${a}0${b}"
 }
 
-fun task2(input: List<String>): Int {
-    return -1
+fun generate(initialState: String, len: Int): String {
+    var state = initialState
+    while (state.length < len) {
+        state = buildNext(state)
+    }
+    return state.substring(0 until len)
+}
+
+fun checksum(s: String): String {
+    if (s.length % 2 == 1) {
+        return s
+    }
+
+    return checksum(s.windowedSequence(2, 2).joinToString(separator = "") {
+        if (it[0] == it[1]) {
+            "1"
+        } else {
+            "0"
+        }
+    })
+}
+
+fun task1(input: List<String>, len: Int): String {
+    val state = input[0]
+    return checksum(generate(state, len))
+}
+
+fun task2(input: List<String>, len: Int): String {
+    return task1(input, len)
 }
--- days/src/test/kotlin/day16/Day16Test.kt
+++ days/src/test/kotlin/day16/Day16Test.kt
@@ -9,17 +9,18 @@ internal class Day16Test {
 
     @Test
     fun testTaskOne() {
-        assertEquals(-1, task1(readPuzzle()))
+        assertEquals("100", buildNext("1"))
+        assertEquals("001", buildNext("0"))
+        assertEquals("11111000000", buildNext("11111"))
+        assertEquals("1111000010100101011110000", buildNext("111100001010"))
+        assertEquals("100", checksum("110010110100"))
+        assertEquals("01100", task1(listOf("10000"), 20))
+
+        assertEquals("11100111011101111", task1(listOf("01110110101001000"), 272))
     }
 
     @Test
     fun testTaskTwo() {
-        assertEquals(-1, task2(readPuzzle()))
-    }
-
-    private fun readPuzzle(): List<String> {
-        val path = Paths.get(this.javaClass.getResource("/day15_puzzle_input.txt").toURI())
-        val readAllLines = Files.readAllLines(path)
-        return readAllLines
+        assertEquals("10001110010000110", task2(listOf("01110110101001000"), 35651584))
     }
 }
