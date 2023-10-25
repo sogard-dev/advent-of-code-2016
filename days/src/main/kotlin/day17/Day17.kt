f5c3fccb25ff6b2ecf9579c055c28fa1550b84e7
apinx <me@apinx.dk>
2023-10-25 20:44:35 +0200

    Day 17

--- days/src/main/kotlin/day17/Day17.kt
+++ days/src/main/kotlin/day17/Day17.kt
@@ -1,9 +1,99 @@
 package day17
 
-fun task1(input: List<String>): Int {
-    return -1
+import day14.toHex
+import java.security.MessageDigest
+import kotlin.math.max
+
+val md = MessageDigest.getInstance("MD5")!!
+
+fun task1(input: List<String>): String {
+    val passcode = input[0]
+
+    val paths = mutableListOf(Triple("", 0, 0))
+    val newPaths: MutableList<Triple<String, Int, Int>> = mutableListOf()
+
+    while (paths.isNotEmpty()) {
+        for ((pathSoFar, row, column) in paths) {
+            if (row == 3 && column == 3) {
+                return pathSoFar
+            }
+
+            val options = options(passcode, pathSoFar)
+            for (movement in options) {
+                val newRow = row + movement.row
+                val newColumn = column + movement.column
+                if (newRow in 0..3 && newColumn in 0..3) {
+                    newPaths.add(Triple(pathSoFar + movement.letter, newRow, newColumn))
+                }
+            }
+        }
+
+        paths.clear()
+        paths.addAll(newPaths)
+        newPaths.clear()
+    }
+
+    return ""
 }
 
 fun task2(input: List<String>): Int {
-    return -1
+    val passcode = input[0]
+
+    val paths = mutableListOf(Triple("", 0, 0))
+    val newPaths: MutableList<Triple<String, Int, Int>> = mutableListOf()
+
+    var longestPath = 0
+
+    for (round in 0..100000) {
+        if (paths.isEmpty()) {
+            return longestPath
+        }
+        for ((pathSoFar, row, column) in paths) {
+            if (row == 3 && column == 3) {
+                longestPath = max(longestPath, round)
+            } else {
+                val options = options(passcode, pathSoFar)
+                for (movement in options) {
+                    val newRow = row + movement.row
+                    val newColumn = column + movement.column
+                    if (newRow in 0..3 && newColumn in 0..3) {
+                        newPaths.add(Triple(pathSoFar + movement.letter, newRow, newColumn))
+                    }
+                }
+            }
+        }
+
+        paths.clear()
+        paths.addAll(newPaths)
+        newPaths.clear()
+    }
+
+    return longestPath
+}
+
+private fun options(passcode: String, path: String): List<Movement> {
+    return md.digest((passcode + path).toByteArray()).toHex()
+        .take(4)
+        .withIndex()
+        .mapNotNull {
+            if (it.value in 'b'..'f') {
+                when (it.index) {
+                    0 -> Movement.UP
+                    1 -> Movement.DOWN
+                    2 -> Movement.LEFT
+                    3 -> Movement.RIGHT
+                    else -> null
+                }
+            } else {
+                null
+            }
+        }
+        .toList()
+}
+
+private enum class Movement(val row: Int, val column: Int, val letter: String) {
+    UP(0, -1, "U"),
+    DOWN(0, 1, "D"),
+    LEFT(-1, 0, "L"),
+    RIGHT(1, 0, "R"),
 }
--- days/src/test/kotlin/day17/Day17Test.kt
+++ days/src/test/kotlin/day17/Day17Test.kt
@@ -9,12 +9,20 @@ internal class Day17Test {
 
     @Test
     fun testTaskOne() {
-        assertEquals(-1, task1(readPuzzle()))
+        assertEquals("DDRRRD", task1(listOf("ihgpwlah")))
+        assertEquals("DDUDRLRRUDRD", task1(listOf("kglvqrro")))
+        assertEquals("DRURDRUDDLLDLUURRDULRLDUUDDDRR", task1(listOf("ulqzkmiv")))
+
+        assertEquals("RDULRDDRRD", task1(listOf("pxxbnzuo")))
     }
 
     @Test
     fun testTaskTwo() {
-        assertEquals(-1, task2(readPuzzle()))
+        assertEquals(370, task2(listOf("ihgpwlah")))
+        assertEquals(492, task2(listOf("kglvqrro")))
+        assertEquals(830, task2(listOf("ulqzkmiv")))
+
+        assertEquals(752, task2(listOf("pxxbnzuo")))
     }
 
     private fun readPuzzle(): List<String> {
