d4a4bebb4f79ec99893b71f31ba99421cbcdaf4b
apinx <me@apinx.dk>
2023-10-29 19:10:05 +0100

    Day 20

--- days/src/main/kotlin/day20/Day20.kt
+++ days/src/main/kotlin/day20/Day20.kt
@@ -1,9 +1,41 @@
 package day20
 
-fun task1(input: List<String>): Int {
-    return -1
+import kotlin.math.max
+
+fun task1(input: List<String>, validFrom: Long, validTo: Long): Long {
+    val toList = input.map { it.split("-").map { n -> n.toLong() } }.map { Pair(it[0], it[1]) }.sortedBy { it.first }.toList()
+    val merged = mergeRanges(toList)
+    if (merged[0].first > validFrom) {
+        return validFrom
+    }
+    return merged[0].second + 1
 }
 
-fun task2(input: List<String>): Int {
-    return -1
+fun task2(input: List<String>, validFrom: Long, validTo: Long): Long {
+    val toList = input.map { it.split("-").map { n -> n.toLong() } }.map { Pair(it[0], it[1]) }.sortedBy { it.first }.toList()
+    val merged = mergeRanges(toList)
+    val range = validTo - validFrom + 1
+    val takenAlready = merged.sumOf { it.second - it.first + 1 }
+    return range - takenAlready
+}
+
+fun mergeRanges(ranges: List<Pair<Long, Long>>): List<Pair<Long, Long>> {
+    val mutList = ranges.toMutableList()
+    var changed = true
+    while (changed) {
+        var i = 0;
+        changed = false
+        while (i < mutList.size - 1) {
+            val cur = mutList[i]
+            val next = mutList[i + 1]
+            if (cur.second >= next.first - 1) {
+                mutList[i] = Pair(cur.first, max(cur.second, next.second))
+                mutList.removeAt(i + 1)
+                changed = true
+            }
+            i++
+        }
+    }
+
+    return mutList
 }
--- days/src/test/kotlin/day20/Day20Test.kt
+++ days/src/test/kotlin/day20/Day20Test.kt
@@ -9,12 +9,14 @@ internal class Day20Test {
 
     @Test
     fun testTaskOne() {
-        assertEquals(-1, task1(readPuzzle()))
+        assertEquals(3, task1(listOf("5-8", "0-2", "4-7"), 0, 9))
+        assertEquals(32259706, task1(readPuzzle(), 0, 4294967295))
     }
 
     @Test
     fun testTaskTwo() {
-        assertEquals(-1, task2(readPuzzle()))
+        assertEquals(2, task2(listOf("5-8", "0-2", "4-7"), 0, 9))
+        assertEquals(113, task2(readPuzzle(), 0, 4294967295))
     }
 
     private fun readPuzzle(): List<String> {
